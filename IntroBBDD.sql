6146667e77a431fb7f83bc936db3fed692aa25c9
Paco <pacocanovaca97@gmail.com>
2026-01-26 14:09:37 +0100

    Ejercicios 2, 3, 4 y 5 hechos

--- IntroBBDD.sql
+++ IntroBBDD.sql
@@ -1,16 +1,26 @@
 -- 1. Escribe una consulta que recupere los Vuelos (flights) y su identificador que figuren con status On Time.
 
-SELECT flight_id, flight_no, status FROM flights WHERE status = 'On Time';
+SELECT flight_id, flight_no FROM flights WHERE status = 'On Time';
 
 -- 2. Escribe una consulta que extraiga todas las columnas de la tabla bookings y refleje todas las reservas que han supuesto una cantidad total mayor a 1.000.000 (Unidades monetarias).
 
+SELECT * FROM bookings WHERE total_amount > 1000000;
+
 -- 3. Escribe una consulta que extraiga todas las columnas de los datos de los modelos de aviones disponibles (aircraft_data). Puede que os aparezca en alguna actualización como "aircrafts_data", revisad las tablas y elegid la que corresponda.
+
+SELECT * FROM aircrafts_data;
 
 -- 4. Con el resultado anterior visualizado previamente, escribe una consulta que extraiga los identificadores de vuelo que han volado con un Boeing 737. (Código Modelo Avión = 733).
 
+SELECT flight_id FROM flights WHERE aircraft_code = '733';
+
 -- 5. Escribe una consulta que te muestre la información detallada de los tickets que han comprado las personas que se llaman Irina.
 
+SELECT * FROM tickets WHERE passenger_name LIKE '%IRINA%'; -- He tenido que buscar en internet cómo introducir un substring en la condición porque no lo encontraba en los apuntes
+
 -- 6. Mostrar las ciudades con más de un aeropuerto.
+
+
 
 -- 7. Mostrar el número de vuelos por modelo de avión.
 
